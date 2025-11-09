15bab62bb7b274a664e6c85262e971478fd451d7
Guilherme Alves Profirio <iamalves.jpg@gmail.com>
2025-11-09 14:48:56 -0300

    feat: setup base character class

---
+++ Source/Aura/Private/Characters/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+
+
+#include "Characters/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
